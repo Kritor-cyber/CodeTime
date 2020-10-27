fbe1a1715e5f90a9f543ae7ab7f1e22debcf50f3
Kritor-cyber <thibault.denier@efrei.net>
2020-10-27 09:21:29 +0100

    Mise a jour des conditions des touches pour faciliter la compréhension

--- main.c
+++ main.c
@@ -34,7 +34,7 @@ int main(void)
         while (continuer)
         {
             Sleep(50);
-            if (GetKeyState(0x53) & 0x8000) // S
+            if (GetKeyState('S') & 0x8000)
             {
                 t = time(NULL);
                 err = localtime_s(&buf, &t);
@@ -46,7 +46,7 @@ int main(void)
                 printf("%dh %dmin %ds\n", buf.tm_hour, buf.tm_min, buf.tm_sec);
                 fprintf_s(timeCode, "%d:%d:%d\n", buf.tm_hour, buf.tm_min, buf.tm_sec);
             }
-            else if (GetKeyState(0x4E) & 0x8000)
+            else if (GetKeyState('N') & 0x8000)
             {
                 continuer = 0;
             }
